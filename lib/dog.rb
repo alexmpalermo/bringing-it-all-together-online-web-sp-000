042155a0130c6e8b6f806f9f36a61382d03becb4
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-28 21:21:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed 
   attr_reader :id 
   
-  def initialize(id: nil, name:, breed:)
+  def initialize(id= nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
@@ -53,7 +53,7 @@ class Dog
   end
   
   def self.new_from_db(row)
-    Dog.create
+    Dog.create(row)
   end 
   
   def self.find_by_name(name)

9b1554070c5fea9dc05d3c1e3cf8dbffbd1c098e
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-28 21:56:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,5 @@
 class Dog 
-  attr_accessor :name, :breed 
-  attr_reader :id 
+  attr_accessor :name, :breed, :id 
   
   def initialize(id: nil, name:, breed:)
     @id = id
@@ -57,6 +56,7 @@ class Dog
   
   def self.new_from_db(row)
     hash = {}
+    hash[:id] = "#{row[0]}"
     hash[:name] = "#{row[1]}"
     hash[:breed] = "#{row[2]}"
     Dog.create(hash)

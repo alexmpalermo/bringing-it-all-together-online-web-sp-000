b6f3ca91a54a4d55e7fc33e7299e9995477bee90
Alexandra Palermo <alexmpalermo@gmail.com>
2019-02-28 21:34:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,7 @@ class Dog
      sql = "SELECT * FROM dogs WHERE id = ?"
     DB[:conn].execute(sql, id).map do |row|
     self.new_from_db(row)
-  end
+  end.first
   end
   
   def self.new_from_db(row)
